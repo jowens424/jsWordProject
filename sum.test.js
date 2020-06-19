772ba92cd1877dc01592c4c908700d760f06a9ba
jackowensii@gmail.com <jackowens@Jacks-MacBook-Pro.local>
2020-06-19 18:16:06 -0400

    Truthy Test

--- sum.test.js
+++ sum.test.js
@@ -2,7 +2,13 @@ const longestWord = require('./test')
 
 test('longestWord is a function', () => {
   expect(typeof longestWord).toEqual('function');
-});
+})
+
+test('"wind and willows" for the longest word', () => {
+  expect(longestWord('willows')).toBeTruthy()
+})
+
+
 
 
 
